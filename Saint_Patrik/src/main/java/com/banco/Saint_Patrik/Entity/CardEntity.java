51635fb5b002b1556f2599bd19f0f69460b6b1f7
Hernán Federico Costarelli <92120975+hernancostarelli@users.noreply.github.com>
2022-02-17 15:32:24 -0300

    CHANGES ON ENTITIES

--- Saint_Patrik/src/main/java/com/banco/Saint_Patrik/Entity/CardEntity.java
+++ Saint_Patrik/src/main/java/com/banco/Saint_Patrik/Entity/CardEntity.java
@@ -1,24 +1,17 @@
 package com.banco.Saint_Patrik.Entity;
 
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class CardEntity {
 
     @Id
